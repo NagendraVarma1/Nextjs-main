eb3e074a969c20bd710997c2df49c7d18654e79a
NagendraVarma1 <nagendravarmal512>
2023-06-23 13:53:58 +0530

    Task 4

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,25 @@
 //our-domain.com/aboutus
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function AboutUs() {
-    return <h1>The About Us Page</h1>
+  return (
+    <Fragment>
+      <h1>The About Us Page</h1>
+      <ul>
+        <li>
+          <Link href="aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="aboutus/3">Suresh</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default AboutUs;
